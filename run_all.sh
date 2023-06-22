655733bb7a72d21576fd0e45c20fe62feddc9c08
Victoria Lena <victoria.magdalena.dax@gmail.com>
2023-06-22 14:27:06 -0700

    Add run file

---
+++ run_all.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -ex
+
+env=bball
+model=imma
+obs_frames=40
+rollouts=9
+randomseed=42
+hidden_dim=256
+edge_types=2 #5
+
+python main.py --env $env --model $model --randomseed $randomseed \
+               --obs_frames $obs_frames --rollouts $rollouts --edge_types $edge_types \
+               --num_epoch 200 --hidden_dim $hidden_dim --lr 1e-6 --plt --long_term --dataset_size 100000
+
+
+env=springs5
+model=imma
+obs_frames=40
+rollouts=9
+randomseed=42
+hidden_dim=256
+edge_types=2
+
+python main.py --env $env --model $model --randomseed $randomseed \
+               --obs_frames $obs_frames --rollouts $rollouts --edge_types $edge_types \
+               --num_epoch 200 --hidden_dim $hidden_dim --lr 1e-6 --plt
+
+
+env=motion
+model=imma
+obs_frames=40
+rollouts=9
+randomseed=42
+hidden_dim=256
+edge_types=2
+
+python main.py --env $env --model $model --randomseed $randomseed \
+               --obs_frames $obs_frames --rollouts $rollouts --edge_types $edge_types \
+               --num_epoch 200 --hidden_dim $hidden_dim --lr 1e-6 --plt
