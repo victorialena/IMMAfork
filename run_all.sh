89c745b73f60db80b71bbe7787f082b009f80db1
Victoria Lena <victoria.magdalena.dax@gmail.com>
2023-06-23 09:23:54 -0700

    Adjust learning metrics

--- run_all.sh
+++ run_all.sh
@@ -1,39 +1,26 @@
 #!/bin/bash -ex
 
-env=bball
 model=imma
 obs_frames=40
 rollouts=9
 randomseed=42
 hidden_dim=256
 edge_types=2 #5
+num_epoch=100
 
-python main.py --env $env --model $model --randomseed $randomseed \
-               --obs_frames $obs_frames --rollouts $rollouts --edge_types $edge_types \
-               --num_epoch 200 --hidden_dim $hidden_dim --lr 1e-6 --plt --long_term --dataset_size 100000
+# env=bball
+# python main.py --env $env --model $model --randomseed $randomseed \
+#                --obs_frames $obs_frames --rollouts $rollouts --edge_types $edge_types \
+#                --num_epoch $num_epoch --hidden_dim $hidden_dim --lr 1e-6 --plt --long_term --dataset_size 100000
 
 
 env=springs5
-model=imma
-obs_frames=40
-rollouts=9
-randomseed=42
-hidden_dim=256
-edge_types=2
-
 python main.py --env $env --model $model --randomseed $randomseed \
                --obs_frames $obs_frames --rollouts $rollouts --edge_types $edge_types \
-               --num_epoch 200 --hidden_dim $hidden_dim --lr 1e-6 --plt
+               --num_epoch $num_epoch --hidden_dim $hidden_dim --lr 1e-6 --plt
 
 
 env=motion
-model=imma
-obs_frames=40
-rollouts=9
-randomseed=42
-hidden_dim=256
-edge_types=2
-
 python main.py --env $env --model $model --randomseed $randomseed \
                --obs_frames $obs_frames --rollouts $rollouts --edge_types $edge_types \
-               --num_epoch 200 --hidden_dim $hidden_dim --lr 1e-6 --plt
+               --num_epoch $num_epoch --hidden_dim $hidden_dim --lr 1e-6 --plt
